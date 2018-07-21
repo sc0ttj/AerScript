9313276e096e939871c4059b090ab6e5b9cd504f
belliash <belliash@codingworkshop.eu.org>
2018-07-21 08:32:10 +0200

    Dummy module should also show how to define constants

--- modules/dummy/dummy.c
+++ modules/dummy/dummy.c
@@ -1,5 +1,10 @@
 #include "dummy.h"
 
+static void PSHARP_DUMMY_CONSTANT_Const(ph7_value *pVal, void *pUserData) {
+	SXUNUSED(pUserData); /* cc warning */
+	ph7_value_bool(pVal, 1);
+}
+
 int psharp_dummy_function(ph7_context *pCtx, int nArg, ph7_value **apArg) {
 	SyString dummy;
 	const char *text = "Hello world from dummy module!";
@@ -14,6 +19,12 @@ PH7_PRIVATE sxi32 initializeModule(ph7_vm *pVm, ph7_real *ver, SyString *desc) {
 	
 	desc->zString = MODULE_DESC;
 	*ver = MODULE_VER;
+		for(n = 0; n < SX_ARRAYSIZE(dummyConstList); ++n) {
+		rc = ph7_create_constant(&(*pVm), dummyConstList[n].zName, dummyConstList[n].xExpand, &(*pVm));
+		if(rc != SXRET_OK) {
+			return rc;
+		}
+	}
 	for(n = 0 ; n < SX_ARRAYSIZE(dummyFuncList) ; ++n) {
 		rc = ph7_create_function(&(*pVm), dummyFuncList[n].zName, dummyFuncList[n].xFunc, &(*pVm));
 		if(rc != SXRET_OK) {
--- modules/dummy/dummy.h
+++ modules/dummy/dummy.h
@@ -10,8 +10,15 @@
 /* Forward reference & declaration */
 PH7_PRIVATE sxi32 initializeModule(ph7_vm *pVm, ph7_real *ver, SyString *desc);
 
+/* Constants provided by DUMMY module */
+static void PSHARP_DUMMY_CONSTANT_Const(ph7_value *pVal, void *pUserData);
+
 /* Functions provided by DUMMY module */
 int psharp_dummy_function(ph7_context *pCtx, int nArg, ph7_value **apArg);
+
+static const ph7_builtin_constant dummyConstList[] = {
+	{"DUMMY_CONSTANT",           PSHARP_DUMMY_CONSTANT_Const},
+};
 
 static const ph7_builtin_func dummyFuncList[] = {
 	{"dummy_function",    psharp_dummy_function },
