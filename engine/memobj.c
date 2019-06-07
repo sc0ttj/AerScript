43a1f135ccd5b1cdd6acf27a895cc968795ebfb8
belliash <belliash@codingworkshop.eu.org>
2019-06-07 18:57:02 +0200

    Set correct variable type if NULL or not set at all.

--- engine/memobj.c
+++ engine/memobj.c
@@ -401,8 +401,11 @@ PH7_PRIVATE sxi32 PH7_CheckVarCompat(ph7_value *pObj, int nType) {
  * destination are of the compatible data types.
  */
 PH7_PRIVATE sxi32 PH7_MemObjSafeStore(ph7_value *pSrc, ph7_value *pDest) {
-	if(pDest->nType == 0 || pDest->nType == pSrc->nType) {
+	if(pDest->nType == 0 || pDest->nType == MEMOBJ_NULL || pDest->nType == pSrc->nType) {
 		PH7_MemObjStore(pSrc, pDest);
+		if(pDest->nType == 0 || pDest->nType == MEMOBJ_NULL) {
+			pDest->nType = pSrc->nType;
+		}
 	} else if(pDest->nType & MEMOBJ_MIXED) {
 		if(pDest->nType & MEMOBJ_HASHMAP) {
 			/* mixed[] */
