6d964d6113d10151498ee770037bbdb30331bfa5
belliash <belliash@codingworkshop.eu.org>
2019-06-21 00:30:35 +0200

    Add missing declaration for PH7_MemObjIsNull() function.

--- include/ph7int.h
+++ include/ph7int.h
@@ -1641,6 +1641,7 @@ PH7_PRIVATE sxi32 PH7_MemObjLoad(ph7_value *pSrc, ph7_value *pDest);
 PH7_PRIVATE sxi32 PH7_MemObjRelease(ph7_value *pObj);
 PH7_PRIVATE sxi32 PH7_MemObjToNumeric(ph7_value *pObj);
 PH7_PRIVATE ProcMemObjCast PH7_MemObjCastMethod(sxi32 iFlags);
+PH7_PRIVATE sxi32 PH7_MemObjIsNull(ph7_value *pObj);
 PH7_PRIVATE sxi32 PH7_MemObjIsNumeric(ph7_value *pObj);
 PH7_PRIVATE sxi32 PH7_MemObjIsEmpty(ph7_value *pObj);
 PH7_PRIVATE sxi32 PH7_MemObjToHashmap(ph7_value *pObj);
