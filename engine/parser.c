843c2d67d927797bc3af059e815b946b0ade58f3
Piotr Likoski <likoski@codingworkshop.eu.org>
2019-07-12 14:36:56 +0200

    I am pretty sure we want to use the number of elements in array in this place, not the actual size of array. The size of a structure is greater than the sum of its parts because of what is called packing. A particular processor has a preferred data size that it works with. Most modern processors' preferred size if 32-bits (4 bytes).

--- engine/parser.c
+++ engine/parser.c
@@ -238,7 +238,7 @@ static sxi32 ExprVerifyNodes(ph7_gen_state *pGen, ph7_expr_node **apNode, sxi32 
 				apNode[i]->pStart->nType &= ~PH7_TK_OCB /*'{'*/;
 				apNode[i]->pStart->nType |= PH7_TK_OSB /*'['*/;
 				pOp = aOpTable;
-				pEnd = &pOp[sizeof(aOpTable)];
+				pEnd = &pOp[sizeof(aOpTable) / sizeof(ph7_expr_op)];
 				while(pOp < pEnd) {
 					if(pOp->iOp == EXPR_OP_SUBSCRIPT) {
 						break;
